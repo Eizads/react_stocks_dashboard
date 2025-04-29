0c98fff7b6bd67ace939764011cec0fb92fd90a2
Sogol Eizadshenass <sogol.izad@gmail.com>
2025-04-29 16:52:27 -0400

    basePath added back in

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   assetPrefix: "/react-stocks-dashboard",
-  // basePath: "/react-stocks-dashboard",
+  basePath: "/react-stocks-dashboard",
   env: {
     NEXT_PUBLIC_BASE_PATH: "/react-stocks-dashboard"
   }
