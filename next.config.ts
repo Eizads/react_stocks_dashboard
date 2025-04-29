34a188428a2176b52aaf509bf98fc237bf77263a
Sogol Eizadshenass <sogol.izad@gmail.com>
2025-04-29 10:44:13 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "/react-stocks-dashboard-static",
+  // basePath: "/react-stocks-dashboard",
 };
 
 export default nextConfig;
