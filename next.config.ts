9ca5aa704644f251f29a7042fcb37e5ebe2c83c7
Sogol Eizadshenass <sogol.izad@gmail.com>
2025-04-29 17:26:58 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   assetPrefix: "/react-stocks-dashboard",
   basePath: process.env.NODE_ENV === 'production' ? '/react-stocks-dashboard' : '',
-  // env: {
-  //   NEXT_PUBLIC_BASE_PATH: "/react-stocks-dashboard"
-  // }
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/react-stocks-dashboard"
+  }
 };
 
 export default nextConfig;
