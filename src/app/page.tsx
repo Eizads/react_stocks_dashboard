1e9959c07fc4c65e6533e66c376f697b098e7a3f
Sogol Eizadshenass <sogol.izad@gmail.com>
2025-04-30 19:25:44 -0400

    mobile pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { BASE_PATH } from "@/config";
 export default function Home() {
   return (
-    <div className="h-full" style={{
+    <div className="h-full overflow-hidden" style={{
               backgroundImage: `url("${BASE_PATH}/banner1.jpg")`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
@@ -9,13 +9,13 @@ export default function Home() {
               
             }}>
                 {/* Content */}
-                <div className="p-9">
-       <div className="relative z-10 flex h-full w-full flex-col items-start justify-start p-9 text-white">
-       <h1 className="text-8xl font-bold leading-tight">
+                <div className="p-9 pt-15 md:pt-9">
+       <div className="relative z-10 flex h-full w-full flex-col items-start justify-start sm:justify-center md:p-9 text-white">
+       <h1 className="text-4xl  md:text-6xl lg:text-8xl font-bold leading-tight">
          Welcome to<br />
           Stocks Dashboard
            </h1>
-           <p className="mt-6 text-3xl">
+           <p className="mt-6 text-lg md:text-xl lg:text-3xl">
              Your one-stop solution for stock market analysis
            </p>
          </div>
